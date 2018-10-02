00eaf26972a3bc0d214a8fea8b9066a6dd7c2c4d
Taariq Hassan <thassan743@gmail.com>
2018-10-02 14:41:32 +0200

    Add template config file

---
+++ config.py
@@ -0,0 +1,17 @@
+# Topline contribution tracker config file
+
+# URL for FNB login page
+fnb_url = "https://www.fnb.co.za/"
+
+# Username and Password for FNB account. For security use only a read-only account.
+# Update two lines below with username and password.
+fnb_username = 'username'
+fnb_password = 'password'
+
+# Driver to use. Either 'chrome' or 'firefox'.
+# If using 'chromedriver' ensure path_to_driver below is correct.
+# If using 'firefoxdriver' ensure driver is installed.
+driver = 'chrome'
+
+# Path to chromedriver executable. Uncomment and enter absolute path if using chromedriver
+path_to_driver = 'C:/path/to/chromedriver/chromedriver.exe'
