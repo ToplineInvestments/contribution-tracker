3537dd961d869bcd7accc3f8c94760b6ba0bd1a1
Taariq Hassan <thassan743@gmail.com>
2018-10-08 20:48:51 +0200

    Change config to use .ini file instead of .py file. Add example config file

--- topline.py
+++ topline.py
@@ -1,17 +1,22 @@
 from topline.scraper import FNB
-import config
+import configparser
+
+config = configparser.ConfigParser()
+if not config.read('config.ini'):
+    print('No config.ini file found')
+    raise SystemExit(0)
 
 path_to_driver = None
 
 try:
-    username = config.fnb_username
-    password = config.fnb_password
-    url = config.fnb_url
-    driver = config.driver
+    username = config['SCRAPER']['USERNAME']
+    password = config['SCRAPER']['PASSWORD']
+    url = config['SCRAPER']['URL']
+    driver = config['SCRAPER']['DRIVER']
     if driver == 'chrome':
-        path_to_driver = config.path_to_driver
-except AttributeError:
-    print("Error getting config from config.py file")
+        path_to_driver = config['SCRAPER']['DRIVER_PATH']
+except KeyError:
+    print("Error getting config from config.ini file")
     raise SystemExit(0)
 
 FNB = FNB(driver=driver, headless=False, driver_path=path_to_driver)
