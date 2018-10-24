e4fc9d791c12b251f3518efdf49316794628875d
Taariq Hassan <thassan743@gmail.com>
2018-10-24 19:54:14 +0200

    Update main script to handle multiple accounts

--- topline.py
+++ topline.py
@@ -8,11 +8,6 @@ if not config.read('config.ini'):
     print('No config.ini file found')
     raise SystemExit(0)
 
-DB = DB(config['DB']['FILENAME'])
-if not DB.connection:
-    print('DB error')
-    raise SystemExit(0)
-
 path_to_driver = None
 
 try:
@@ -22,8 +17,15 @@ try:
     driver = config['SCRAPER']['DRIVER']
     if driver == 'chrome':
         path_to_driver = config['SCRAPER']['DRIVER_PATH']
+    db_file = config['DB']['FILENAME']
+    excel_file = config['EXCEL']['FILENAME']
 except KeyError:
     print("Error getting config from config.ini file")
+    raise SystemExit(0)
+
+DB = DB(db_file)
+if not DB.connection:
+    print('DB error')
     raise SystemExit(0)
 
 FNB = FNB(driver=driver, headless=False, driver_path=path_to_driver)
@@ -36,9 +38,20 @@ if FNB.driver:
     else:
         FNB.driver.quit()
 
-excel = Excel(config['EXCEL']['FILENAME'])
+excel = Excel(excel_file)
 if excel.workbook:
-    excel.get_sheets()
     for account in FNB.accounts:
-        excel.update_excel(FNB.accounts[account]['transactions'])
-    excel.close_workbook()
+        print("Processing transactions in account: {}".format(FNB.accounts[account]['name']))
+        count = 0
+        if 'transactions' in FNB.accounts[account]:
+            for trans in FNB.accounts[account]['transactions']:
+                if excel.add_transaction(FNB.accounts[account]['name'], FNB.accounts[account]['acc_num'], trans):
+                    count += 1
+            print("Processed {}/{} transactions in account: {}".format(count,
+                                                                       len(FNB.accounts[account]['transactions']),
+                                                                       FNB.accounts[account]['name']))
+        else:
+            print('No transactions for account: {}'.format(FNB.accounts[account]['name']))
+    excel.close_workbook(overwrite=False, filename='master_update.xlsx')
+
+print("Done")
